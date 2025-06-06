564dbbf7c8b395da1cd8c76f076cc406bb3381c2
gruchh <kubol2@gmail.com>
2025-06-06 21:33:11 +0200

    Frontend init with template menu

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import NavMenu from "./components/navigation/NavMenu";
+
+function App() {
+  return (
+    <div className="min-h-screen grid grid-cols-12 bg-gray-100 m-0">
+      <div className="col-span-3 h-screen hidden md:block">
+        <NavMenu />
+      </div>
+      <div className="col-span-12 md:col-span-9 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-800">Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
