0d8a289ab5c234283053b682ab907476f57bd33f
gruchh <kubol2@gmail.com>
2025-06-08 11:33:04 +0200

    Login Page init

--- Frontend/src/Routes/AppRoutes.jsx
+++ Frontend/src/Routes/AppRoutes.jsx
@@ -1,14 +1,15 @@
 import App from "../App";
+import LoginPage from "../components/Auth/LoginPage";
 import Dashboard from "../components/Dashboard/Dashboard";
 import NotFound from "../components/NotFound/NotFound";
-import { Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from "react-router-dom";
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
       <Route element={<App />}>
-        <Route path="/" element={< Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
